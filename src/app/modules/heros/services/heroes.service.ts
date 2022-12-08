0654311a3fdc7698bd6e3a4144f422d82dc9d6fd
Manuel Ulate <fr3zs09@gmail.com>
2022-12-08 16:39:07 -0600

    uso de variable de entorno en servicios

--- src/app/modules/heros/services/heroes.service.ts
+++ src/app/modules/heros/services/heroes.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { HerosResponse } from '../interfaces/heros.interface';
 
 @Injectable({
@@ -8,14 +9,16 @@ import { HerosResponse } from '../interfaces/heros.interface';
 })
 export class HeroesService {
 
+  private baseUrl:string = environment.baseUrl;
+
   constructor( private http:HttpClient ) { }
 
   getHeros():Observable<HerosResponse[]> {
-    return this.http.get<HerosResponse[]>('http://localhost:3000/heroes');
+    return this.http.get<HerosResponse[]>(`${ this.baseUrl }/heroes`);
   }
 
   getHeroById( id:string ):Observable<HerosResponse> {
-    return this.http.get<HerosResponse>(`http://localhost:3000/heroes/${ id }`);
+    return this.http.get<HerosResponse>(`${ this.baseUrl }/heroes/${ id }`);
   }
 
 }
