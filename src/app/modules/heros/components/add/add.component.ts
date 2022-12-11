cac95d52ec2575b6bd271c46bf632cf1254bbef6
Manuel Ulate <fr3zs09@gmail.com>
2022-12-10 20:20:09 -0600

    pantalla de agregar heroe

--- src/app/modules/heros/components/add/add.component.ts
+++ src/app/modules/heros/components/add/add.component.ts
@@ -1,10 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { HerosResponse, Publisher } from '../../interfaces/heros.interface';
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html'
 })
 export class AddComponent implements OnInit {
+
+  public publishers = [
+    {
+      id: 'DC Comics',
+      name: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      name: 'Marvel - Comics'
+    }
+  ]
+
+  public hero:HerosResponse = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: ''
+  }
 
   constructor() { }
 
