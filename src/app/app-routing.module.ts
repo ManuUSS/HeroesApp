4a7bbfbb61ca20c2982682d4a335334db919b570
Manuel Ulate <fr3zs09@gmail.com>
2022-12-12 14:51:02 -0600

    proteccion mediante el auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './modules/auth/guards/auth.guard';
 
 import { ErrorPageComponent } from './shared/pages/error-page/error-page.component';
 
@@ -11,7 +12,8 @@ const routes: Routes = [
   },
   {
     path: 'heros',
-    loadChildren: () => import('./modules/heros/heros.module').then( m => m.HerosModule )
+    loadChildren: () => import('./modules/heros/heros.module').then( m => m.HerosModule ),
+    canLoad: [ AuthGuard ]
   },
   {
     path: '404',
--- src/app/modules/auth/guards/auth.guard.ts
+++ src/app/modules/auth/guards/auth.guard.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
+
+  constructor( private authService:AuthService ) { }
+
   //canActivate(
   //  route: ActivatedRouteSnapshot,
   //  state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
@@ -14,6 +18,8 @@ export class AuthGuard implements CanLoad {
   canLoad(
     route: Route,
     segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+      if( this.authService.auth.id )
+        return true;
+      return false;
   }
 }
